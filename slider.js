58eeaeb15e781f6b894e656bf2ff4fe4211d8dce
pjs <vkrwltn12@gmail.com>
2022-12-06 20:46:59 +0900

    reduce code clean

--- slider.js
+++ slider.js
@@ -2,24 +2,24 @@ const left = document.querySelector(".left")
 const right = document.querySelector(".right")
 const slider = document.querySelector(".slider")
 const images = document.querySelectorAll(".img")
+const legnth = images.length
 
 let slideNumber = 1
 
+const nextSlide = ()=>{
+    if (slideNumber < legnth){
+        slider.style.transform = `translateX(-${slideNumber * 800}px)`;
+            slideNumber++;}
+}
+
+const firstSlide = ()=>{
+    slider.style.transform = `translateX(0px)`;
+    slideNumber = 1;
+}
 
 right.addEventListener("click",()=>{
-    if (slideNumber < images.length){
-    slider.style.transform = `translateX(-${slideNumber * 800}px)`;
-        slideNumber++;
-    }else {
-        slider.style.transform = `translateX(0px)`;
-        slideNumber = 1;
-    }
+    slideNumber < legnth ? nextSlide() : firstSlide()
+    
+})
 
-}
-)
-
-left.addEventListener("click",()=>{
-    slider.style.transform = `translateX(+800px)`;
-}
-)
 
